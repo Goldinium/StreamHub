b1ec3976fe43a448e776f922b15a128559829d78
Badr BenSlama <badrbenslama8@gmail.com>
2023-09-04 15:16:13 +0100

    assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -15,6 +16,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
 
+    }),
+    new CopyPlugin({
+      patterns: [
+        
+        { from: 'src/assets', to: 'assets' },
+    ],
     }),
   ],
   output: {
